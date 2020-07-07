1649948b01e93d710b4b3002f5023043406b430f
Krissy <krissysaenz@gmail.com>
2020-07-07 14:37:16 -0700

    animated background added

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,11 +7,15 @@ function LandingPage(){
     return(
         <div className="container">
             <AnimatedBackground />
-            <img className="landing-logo" src="/logo.png" alt="logo"/>
+            <div className="logo-container">
+                <img className="landing-logo" src="/logo.png" alt="logo"/>
+                <p className="logo-text">Play With Me</p>
+            </div>
             <div className="landing-info">
                 <h1>Find Your New Playmate!</h1>
                 <p>Play With Me is an app thats helps you find playdates for your children. To get started simply browse current playdate events or create your own!</p>
-                <Link to="/results">Get Started</Link>
+                {/*<Link to="/results">Get Started</Link>*/}
+                <button className="myButton" to="/results" renderas={Link}>Get Started</button>
             </div>
         </div>
     )
